295d7909ce55663985160ce85d4f2e6790619365
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-06-17 17:41:24 +0530

    added web page

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var moment = require('moment');
+var path = require('path');
 
 var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.set({'Content-Type': 'text/html'});
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+});
 
 router.get('/:timestampQuery', function(req, res) {
   var timestamp = req.params.timestampQuery;
